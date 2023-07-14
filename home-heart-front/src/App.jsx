58fc5fa75651fd7af6c23298fdbe489e2d660a17
Ethan Pineda <joshuapineda66@gmail.com>
2023-07-14 10:13:43 -0700

    login form component done

--- home-heart-front/src/App.jsx
+++ home-heart-front/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from './components/RegistrationForm/RegistrationForm'
+import LoginForm from './components/LoginForm/LoginForm'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
 
@@ -13,8 +12,9 @@ function App() {
     <>
     <Router>
       <Routes>
-      <Route path="/register" element={<RegistrationForm />}></Route>
-      <Route path="/" element={<NavBar />}></Route>
+      <Route path="/register" element={<RegistrationForm />} /> 
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/" element={<NavBar />} />
       </Routes>
      </Router>
 
