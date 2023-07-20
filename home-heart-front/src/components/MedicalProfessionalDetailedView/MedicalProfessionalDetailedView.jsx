0d02482bf86a9335a3b2636fbf2340ed124e78b5
Ethan Pineda <joshuapineda66@gmail.com>
2023-07-19 23:04:32 -0700

    medical professionals detailed view 50% done

--- home-heart-front/src/components/MedicalProfessionalDetailedView/MedicalProfessionalDetailedView.jsx
+++ home-heart-front/src/components/MedicalProfessionalDetailedView/MedicalProfessionalDetailedView.jsx
@@ -1,12 +1,66 @@
 import React from "react";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from "@mui/material/Button";
 
 const MedicalProfessionalDetailedView = () => {
 
 
-    return(
-        <div>
-            MedicalProfessionalDetailedView
-        </div>
+    // const { name, image, bio, country, languages, yearsOfExperience, specialization, education } = professional;
+
+    return (
+        <Container>
+            <Grid container spacing={3} alignItems="center">
+                {/* Left Half: Medical Professional Image and Name */}
+                <Grid item xs={12} md={6}>
+                    <img 
+                        src={"https://t3.ftcdn.net/jpg/02/60/04/08/360_F_260040863_fYxB1SnrzgJ9AOkcT0hoe7IEFtsPiHAD.jpg"} 
+                        alt={name} 
+                        style={{ width: "100%", height: "auto", borderRadius: "8px"}} /> 
+                    <Typography variant="h4" gutterBottom>{"Ethan Pineda"}</Typography>
+                    <Typography variant="h6" gutterBottom>${"5"} per session</Typography>
+                </Grid>
+
+                {/* Right Half: Bio and Details */}
+                <Grid item xs={12} md={6}>
+                    <Typography variant="body1">
+                        {"Meet Dr. Emily, a compassionate mental health professional with a profound commitment to healing and supporting individuals. With extensive experience in therapy and research, she empowers her patients to embrace positive change and achieve lasting well-being."}
+                    </Typography>
+                    <Typography variant="h6">
+                        Country: {"Kenya"}
+                    </Typography>
+                    {/* <Typography variant="h6">
+                        Languages: {languages.join(", ")}
+                    </Typography> */}
+                    <Typography variant="h6">
+                        Years of Experience: {"2"}
+                    </Typography>
+                    {/* <Typography variant="h6">
+                        Specialization: {specialization}
+                    </Typography> */}
+                    {/* <Typography variant="h6">
+                        Education: {education}
+                    </Typography> */}
+                    <Button 
+                        variant="contained" 
+                        size="small" 
+                        color="primary" 
+                        style={{ marginTop: "1rem" }}
+                        >
+                            Book Appointment
+                    </Button>
+                    <Button 
+                        variant="contained" 
+                        size="small"  
+                        color="primary" 
+                        style={{ marginTop: "1rem" }}
+                        >
+                            Send Message
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
--- home-heart-front/src/components/MedicalProfessionalsGrid/MedicalProfessionalCard.jsx
+++ home-heart-front/src/components/MedicalProfessionalsGrid/MedicalProfessionalCard.jsx
@@ -1,40 +1,41 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import Bookmark from '@mui/icons-material/Bookmark';
 import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Button from '@mui/material/Button';
+import { Link as RouterLink } from "react-router-dom"; 
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+
+const useStyles = makeStyles((theme) => ({
+    learnMore: {
+        backgroundColor: "#7693B0",
+        fontFamily: 'Inter, sans-serif',
+    }, 
+    onHover: {
+        backgroundColor: "#506d8a",
+    }
+})); 
 
 export default function MedicalProfessionalCard({ professional }) {
-    
-  const [expanded, setExpanded] = React.useState(false);
 
-  const { name, image, country, language, modality, bio } = professional;
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+    const classes = useStyles();
+
+    const { name, image, country, language, modality, bio } = professional;
+
+    // TODO: Implement this
+    const handleMedicalProfessionalBookmarked = () => {
+        console.log("Medical professional bookmarked");
+    }
+
 
   return (
     <Card sx={{ maxWidth: 345 }}>
@@ -60,49 +61,19 @@ export default function MedicalProfessionalCard({ professional }) {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+            <Bookmark 
+            />
         </IconButton>
-        <IconButton aria-label="share">
+        <IconButton aria-label="share"  onClick={handleMedicalProfessionalBookmarked}>
           <ShareIcon />
         </IconButton>
-        {/* <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore> */}
+        {/* TODO: Add the correct Route here */}
+        <RouterLink to="">
+            <Button variant='contained' size="small" className={classes.learnMore}>
+                Learn More
+            </Button>
+        </RouterLink>
       </CardActions>
-      {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse> */}
     </Card>
   );
 }
