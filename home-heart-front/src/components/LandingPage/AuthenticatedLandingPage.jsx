cc585551364c59d47b6efb8098193e57a49c2d13
Richmond Akondo <rakondo@rakondo-ltm996a.internal.salesforce.com>
2023-08-01 20:40:25 -0700

    worked on some error handling for userform

--- home-heart-front/src/components/LandingPage/AuthenticatedLandingPage.jsx
+++ home-heart-front/src/components/LandingPage/AuthenticatedLandingPage.jsx
@@ -5,13 +5,15 @@ import ImageCardGetStarted from "../GetStartedPage/GetStartedPage";
 import HowItWorks from "./HowItWorks";
 import HowItWorksAuth from "./HowItWorksAuth";
 
-export default function AuthenticatedLandingPage({user}) {
+export default function AuthenticatedLandingPage({user, userData}) {
+  console.log(user.userId)
   return (
     <div>
-      <ImageCardGetStarted user={user} />
+      <ImageCardGetStarted user={user} userData={userData} />
       {/*  to be replaced by modified HowItWorks for auth-ed users later on */}
       <HowItWorksAuth />
       <Features />
     </div>
   );
 }
+
